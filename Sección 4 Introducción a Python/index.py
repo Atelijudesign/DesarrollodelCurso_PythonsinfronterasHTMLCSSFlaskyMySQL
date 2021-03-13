e2ef6310d4b32df3a736b0adbb7ed877f9a70fed
Andrés Gallo <atelijudesign@gmail.com>
2021-03-13 19:22:48 -0300

    Carpetas del curso

---
+++ Sección 4 Introducción a Python/index.py
@@ -0,0 +1,22 @@
+#Indentacion en python
+if 10 > 6:
+    print ("10 es mayor")
+
+#Variables
+x = 5
+y = "Texto"
+print(x,y)
+email= "andresgallo@wps.com"
+print(email)
+#definir multiples variables en una linea
+a,b,c= "andres", "Gallo", "Parra"
+print(a,b,c)
+
+#Definir variables con el mismo valor
+valor1=valor2=valor3="Andrés Gallo"
+print(valor1,valor2,valor3)
+
+#Concatenar variables
+inicio = "Hola"
+final = "Mundo"
+print (inicio+final)
